21cf196015f5144c232eec7ea968fc036f7cff42
Nazar <bonasane@gmail.com>
2024-07-08 20:24:17 +0300

    cleaned

--- case.py
+++ case.py
@@ -1,4 +1,19 @@
 import pandas as pd
 df = pd.read_csv('DataAnalyst.csv')
+
 df.info()
+
+
+def salary_set_min(salary):
+    global df
+    if salary[0] == '$':
+        df = df.replace({'$': ''})
+
+    else:
+        return -1
+
+df['Salary Estimate'] = df['Salary Estimate'].apply(salary_set_min)
+df.to_csv('Cleaned.csv')
+
+
 
