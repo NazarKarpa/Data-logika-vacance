95951f7f6bc7ac47dcd539fcc8513a614f74066a
Nazar <bonasane@gmail.com>
2024-07-10 10:59:58 +0300

    cleaned salary

--- case.py
+++ case.py
@@ -4,15 +4,26 @@ df = pd.read_csv('DataAnalyst.csv')
 df.info()
 
 
-def salary_set_min(salary):
+def salary_set_cleaned(salary):
+
+    if '$' in salary:
+        salary = salary.replace('$', '')
+    if '-' in salary:
+        salary = salary.replace('-', '')
+    if '(Glassdoor est.)' in salary:
+        salary = salary.replace('(Glassdoor est.)', '')
+    if 'K' in salary:
+        salary = salary.replace('K', '')
+    return salary
+def group_salary(salary):
     global df
-    if salary[0] == '$':
-        df = df.replace({'$': ''})
 
-    else:
-        return -1
+    for df['Salary Estimate'] in range(2253):
+        df.insert(2, 'Min_salary', 1)
 
-df['Salary Estimate'] = df['Salary Estimate'].apply(salary_set_min)
+df = group_salary(['Salary Estimate'])
+
+df['Salary Estimate'] = df['Salary Estimate'].apply(salary_set_cleaned)
 df.to_csv('Cleaned.csv')
 
 
